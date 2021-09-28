4d323430eff1ed58e3dc352b502335d5e929fca0
tagzara <tagzara82@gmail.com>
2021-09-28 13:23:02 +0300

    add express handlebars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 
+// this is set the absolute way to images folder
+app.set('views', path.resolve('./src/views'));
+app.engine('hbs', handlebars({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+
 app.all('/', (req, res) => {
-    res.write('It\'s working')
-    res.end();
+    res.render('index', { layout: false });
 });
 
 app.listen(5000, console.log.bind(console, 'Application is running on http://localhost:5000'));
