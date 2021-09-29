2afcbdea5286c7864fd0b98356d013d1925de3f6
tagzara <tagzara82@gmail.com>
2021-09-29 10:18:21 +0300

    create cube

--- src/models/cube.js
+++ src/models/cube.js
@@ -1,9 +1,29 @@
+const uniqid = require('uniqid');
+
 class Cube {
+    static cubes = [
+        {
+            id: 'asjdhikjnhjnlkl8',
+            name: 'Mirror Cube',
+            description: 'Strange Cube',
+            imageUrl: 'http://cdn.shopify.com/s/files/1/0270/0342/0758/products/qiyi-3x3-mirror-cube-silver-2690-d_e59715aa-1350-4bbf-80be-8ae16f035dab_1024x.jpg?v=1610959715',
+            difficulty: '4'
+        }
+    ]
     constructor (name, description, imageUrl, difficulty) {
+        this.id = uniqid();
         this.name = name;
         this.description = description;
         this.imageUrl = imageUrl;
         this.difficulty = difficulty;
+    }
+
+    static getAll() {
+        return Cube.cubes.slice();
+    }
+
+    static add(cube) {
+        Cube.cubes.push(cube);
     }
 }
 
--- src/services/cubService.js
+++ src/services/cubService.js
@@ -1,18 +1,17 @@
 const Cube = require('../models/cube');
 
-const cubeDb = [];
 
-const getAll = () => cubeDb.slice();
+const getAll = () => Cube.getAll();
 
 const create = (name, description, imageUrl, difficulty) => {
     let cube = new Cube(name, description, imageUrl, difficulty);
 
-    cubeDb.push(cube);
+    Cube.add(cube);
 };
 
 const cubeService = {
-    create,
-    getAll
+    getAll,
+    create
 };
 
 module.exports = cubeService;
